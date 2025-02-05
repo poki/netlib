14f8685bc0724c1f60f2c7a3c8d2f256ddf0117f
Erik Dubbelboer <erik@dubbelboer.com>
2025-02-05 05:16:19 +0100

    Ping the signaling server from the client
    Send pings to be able to detect broken connections. No need for a pong,
    TCP will take care of that.
    
    Log the origin with "unknown packet type received" so we can see which
    clients are sending packets the server doesn't support (in this case it
    was ping from old clients?).

--- internal/signaling/peer.go
+++ internal/signaling/peer.go
@@ -17,6 +17,8 @@ import (
 
 const DefaultMaxPlayers = 4
 
+var ErrUnknownPacketType = fmt.Errorf("unknown packet type")
+
 type Peer struct {
 	store stores.Store
 	conn  *websocket.Conn
@@ -172,7 +174,7 @@ func (p *Peer) HandlePacket(ctx context.Context, typ string, raw []byte) error {
 		}
 
 	default:
-		logger.Warn("unknown packet type received", zap.String("type", typ))
+		return ErrUnknownPacketType
 	}
 
 	return nil

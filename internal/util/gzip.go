7ab753a57032f0c8b171b4dc60cedfd280a2d119
Pedr0Rocha <pedrovqrocha@gmail.com>
2025-12-18 16:39:20 +0100

    gzip notify/listen payloads

---
+++ internal/util/gzip.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io"
+)
+
+func GzipCompress(input []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	writer, err := gzip.NewWriterLevel(&buf, gzip.BestSpeed)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = writer.Write(input)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := writer.Close(); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func GzipDecompress(compressedInput []byte) ([]byte, error) {
+	reader, err := gzip.NewReader(bytes.NewReader(compressedInput))
+	if err != nil {
+		return []byte{}, err
+	}
+	defer reader.Close()
+
+	decompressed, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return decompressed, nil
+}
